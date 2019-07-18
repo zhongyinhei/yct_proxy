a44188faaa28c70e1f263feae315be08578be807
“徐金金” <13776572414@163.com>
2019-07-18 16:04:46 +0800

    test mysql&redis

--- handle_data/celery_config.py
+++ handle_data/celery_config.py
@@ -17,13 +17,13 @@ from raven import Client
 cli = Client('https://6bc40853ade046ebb83077e956be04d2:d862bee828d848b6882ef875baedfe8c@sentry.cicjust.com//5')
 
 
-# SURL = "mysql+pymysql://cic_admin:TaBoq,,1234@192.168.1.170:3306/yct_proxy?charset=utf8&autocommit=true"
-SURL = "mysql+pymysql://cic_admin:159357a@192.168.10.11:3306/yct_proxy?charset=utf8&autocommit=true"
+SURL = "mysql+pymysql://cic_admin:TaBoq,,1234@192.168.1.170:3306/yct_proxy?charset=utf8&autocommit=true"
+# SURL = "mysql+pymysql://cic_admin:159357a@192.168.10.11:3306/yct_proxy?charset=utf8&autocommit=true"
 # SURL = "mysql+pymysql://root:GHys1234&,><@!@192.168.10.246:3306/yct_proxy?charset=utf8&autocommit=true"
 
 
-REDIS_HOST = 'aliyun_redis'
-# REDIS_HOST = '127.0.0.1'
+# REDIS_HOST = 'aliyun_redis'
+REDIS_HOST = '127.0.0.1'
 REDIS_PORT = 6379
 
 
